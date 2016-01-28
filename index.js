8e3cbcfef7d2ed58ce0604af1b6bff107f90609e
StefanoMagrassi <stefano.magrassi@gmail.com>
2016-01-28 17:19:33 +0100

    render as module with tests

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 // Imports
 // -------
-import _partial from 'lodash/partial';
-import pieces from './lib/pieces';
+import render from './lib/render';
 
 let image = document.getElementById('img-puzzle_img');
-
-function render(img, rows, cols) {
-  let imageW = img.width;
-  let imageH = img.height;
-  let pieceW = imageW / cols;
-  let pieceH = imageH / rows;
-
-  return pieces(rows, cols, pieceW, pieceH)
-          .forEach(_partial(addPiece, img.parentNode));
-}
-
-//console.log(pieces(r, c, pieceW, pieceH));
-
-function addPiece(container, item) {
-  let piece = document.createElement('span');
-  piece.className = 'img-puzzle_piece';
-  piece.innerHTML = item.index;
-  piece.style.width = item.width + 'px';
-  piece.style.height = item.height + 'px';
-  piece.style.left = item.oriX + 'px';
-  piece.style.top = item.oriY + 'px';
-
-  container.appendChild(piece);
-}
 
 render(image, 3, 3);
