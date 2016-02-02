51ecd7e1bb5bf563147715f0cca2f02c685e993e
StefanoMagrassi <stefano.magrassi@gmail.com>
2016-02-02 18:18:30 +0100

    grid only as representation of cells

--- tests/grid.js
+++ tests/grid.js
@@ -5,20 +5,13 @@ import grid from '../lib/grid';
 
 // Tests
 // -----
-test('grid() returns a collection of cells', function(t) {
+test('grid() returns a representation of a grid', function(t) {
   let rows   = 2;
   let cols   = 2;
-  let w      = 100;
-  let h      = 100;
-  let actual = grid(rows, cols, w, h);
-  let expect = [
-    {row:0, col:0, width:w, height:h, x: 0, y:0},
-    {row:0, col:1, width:w, height:h, x: w, y:0},
-    {row:1, col:0, width:w, height:h, x: 0, y:h},
-    {row:1, col:1, width:w, height:h, x: w, y:h}
-  ];
+  let actual = grid(rows, cols);
+  let expect = [[0, 0], [0, 1], [1, 0], [1, 1]];
 
-  t.deepEqual(actual, expect, 'should returns a collection of cells');
+  t.deepEqual(actual, expect, 'should returns a collection of pairs that represent the cells of a grid');
 
   t.end();
 });
