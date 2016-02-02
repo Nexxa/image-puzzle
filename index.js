cb1d5979a8b12e5a5d01560c1d9d734c20d68c24
StefanoMagrassi <stefano.magrassi@gmail.com>
2016-02-02 16:37:44 +0100

    image puzzle factory

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+/**
+ * @file Image Puzzle - puzzle game factory
+ * @author StefanoMagrassi <stefano.magrassi@gmail.com>
+ */
+
 // Imports
 // -------
-import render from './lib/render';
+import _partial from 'lodash/partial';
+import {default as render, update, last} from './lib/render';
 
 // Exports
 // -------
-export default render;
+export default imagePuzzle;
+
+// Public methods
+// --------------
+function imagePuzzle(image, rows, cols) {
+  return {
+    image : image,
+    rows  : rows,
+    cols  : cols,
+    render: _partial(render, image, rows, cols),
+    update: _partial(update, image, rows, cols),
+    last  : last
+  };
+}
