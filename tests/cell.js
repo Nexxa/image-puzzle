8c4f3f8e69ced1fd6d05486e208701eee2ae2763
StefanoMagrassi <stefano.magrassi@gmail.com>
2016-02-03 12:45:09 +0100

    collection holds cells and items

--- tests/cell.js
+++ tests/cell.js
@@ -1,32 +1,28 @@
 /**
- * @file Piece object tests
+ * @file Cell module - tests
  * @author StefanoMagrassi <stefano.magrassi@gmail.com>
  */
 
 // Imports
 // -------
 import test from 'tape';
-import item from '../lib/item';
+import item from '../lib/cell';
 
 // Tests
 // -----
-test('item() returns an "item" object', function(t) {
-  let index  = 0;
+test('cell() returns a "cell" object', function(t) {
   let row    = 0;
   let col    = 1;
   let width  = 100;
   let height = 100;
-  let actual = item(index, row, col, width, height);
+  let actual = item(width, height, row, col);
   let expect = {
-    position: index,
     row     : row,
     col     : col,
     width   : width,
     height  : height,
     x       : 100,
-    y       : 0,
-    bgX     : -100,
-    bgY     : 0
+    y       : 0
   };
 
   t.deepEqual(actual, expect, 'should returns an item object');
