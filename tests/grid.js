3f872c6f9c7327db47f2f7e101ae23fad5ac2502
StefanoMagrassi <stefano.magrassi@gmail.com>
2016-03-08 11:53:59 +0100

    tests -> grid

--- tests/grid.js
+++ tests/grid.js
@@ -10,20 +10,20 @@ import grid from '../lib/grid';
 
 // Tests
 // -----
-test('grid() returns a representation of a grid', function(t) {
-  let w      = 200;
-  let h      = 200;
-  let rows   = 2;
-  let cols   = 2;
-  let actual = grid(w, h, rows, cols);
-  let expect = [
+test('Lib: grid', t => {
+  const w    = 200;
+  const h    = 200;
+  const rows = 2;
+  const cols = 2;
+
+  const actual = grid(w, h, rows, cols);
+  const expect = [
     {row:0, col:0, width:100, height:100, x:0, y:0},
     {row:0, col:1, width:100, height:100, x:100, y:0},
     {row:1, col:0, width:100, height:100, x:0, y:100},
     {row:1, col:1, width:100, height:100, x:100, y:100}
   ];
 
-  t.deepEqual(actual, expect, 'should returns a collection of pairs that represent the cells of a grid');
-
+  t.deepEqual(actual, expect, 'should return a collection of pairs that represent the cells of a grid');
   t.end();
 });
