213da19dfae0e4b85ef16c6225bf794dc3b460cc
StefanoMagrassi <stefano.magrassi@gmail.com>
2016-03-08 11:49:26 +0100

    tests -> cell

--- tests/cell.js
+++ tests/cell.js
@@ -10,22 +10,22 @@ import cell from '../lib/cell';
 
 // Tests
 // -----
-test('cell() returns a "cell" object', function(t) {
-  let row    = 0;
-  let col    = 1;
-  let width  = 100;
-  let height = 100;
-  let actual = cell(width, height, row, col);
-  let expect = {
-    row     : row,
-    col     : col,
-    width   : width,
-    height  : height,
-    x       : 100,
-    y       : 0
+test('Lib: cell', t => {
+  const row    = 0;
+  const col    = 1;
+  const width  = 100;
+  const height = 100;
+
+  const actual = cell(width, height, row, col);
+  const expect = {
+    row   : row,
+    col   : col,
+    width : width,
+    height: height,
+    x     : 100,
+    y     : 0
   };
 
   t.deepEqual(actual, expect, 'should returns a cell object');
-
   t.end();
 });
